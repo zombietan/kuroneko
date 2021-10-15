bff49e5aca61e3e3812a7edb367ecaaa028edbe6
fatcat7 <fatcat7@aol.jp>
2021-10-16 01:17:36 +0900

    fix test

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -35,7 +35,7 @@ func TestNoArgs(t *testing.T) {
 	}
 }
 
-func TestOverSerial(t *testing.T) {
+func TestTooManySerial(t *testing.T) {
 	color.NoColor = true
 	outBuf := new(bytes.Buffer)
 	errBuf := new(bytes.Buffer)
@@ -49,7 +49,7 @@ func TestOverSerial(t *testing.T) {
 	}
 }
 
-func TestFewrSerial(t *testing.T) {
+func TestNotEnoughSerial(t *testing.T) {
 	color.NoColor = true
 	outBuf := new(bytes.Buffer)
 	errBuf := new(bytes.Buffer)
@@ -60,5 +60,66 @@ func TestFewrSerial(t *testing.T) {
 	errMessage := "Error: 連番で取得できるのは 1~10件 までです\n"
 	if errMessage != errBuf.String() {
 		t.Fatal("failed test2 FewSerial")
+	}
+}
+
+func TestRemoveHyphen(t *testing.T) {
+	var tests = []struct {
+		input    string
+		expected string
+	}{
+		{"foo-", "foo"},
+		{"-bar", "bar"},
+		{"baz", "baz"},
+		{"000-0000-0000", "00000000000"},
+	}
+
+	for _, test := range tests {
+		output := removeHyphen(test.input)
+		if output != test.expected {
+			t.Errorf("Test Faild: input %v, got %v, want %v", test.input, output, test.expected)
+		}
+	}
+
+}
+
+func TestIsInt(t *testing.T) {
+	var tests = []struct {
+		input    string
+		expected bool
+	}{
+		{"123456789012", true},
+		{"00000000000a", false},
+		{"000-000-0000", false},
+		{"00.000000000", false},
+		{"000000000000", true},
+		{"000000000001", true},
+		{"00000000000１", true},
+	}
+
+	for _, test := range tests {
+		output := isInt(test.input)
+		if output != test.expected {
+			t.Errorf("Test Faild: input %v, got %v, want %v", test.input, output, test.expected)
+		}
+	}
+}
+
+func TestIs12or11Digits(t *testing.T) {
+	var tests = []struct {
+		input    string
+		expected bool
+	}{
+		{"123456789012", true},
+		{"12345678901", true},
+		{"００００００００００００", false},
+		{"０００００００００００", false},
+	}
+
+	for _, test := range tests {
+		output := is12or11Digits(test.input)
+		if output != test.expected {
+			t.Errorf("Test Faild: input %v, got %v, want %v", test.input, output, test.expected)
+		}
 	}
 }
